633163e6a29727ab67f62f63089b33e03a098f70
igurrola6 <igurrola6@gmail.com>
2021-06-03 00:20:18 -0700

    Testing Fomantic-UI

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 $(document).ready(function() { // Makes sure that your function is called once all the DOM elements of the page are ready to be used.
     $('.hide').click(clickedHide);
 	$('.show').click(clickedShow);
+	$('.invert').click(clickedInverted);
   })
+  var inverted = true;
   $(document).on('click', '#a', function() { alert("hello"); });
   
    function clickedHide() {
@@ -10,5 +12,26 @@ $(document).ready(function() { // Makes sure that your function is called once a
     }
    function clickedShow() {
       $( "#a" ).show();
+    }
+	
+   //this is for testing, it is much better to switch css files, one for dark mode and one for light mode 
+   function clickedInverted() {
+	  if (inverted == true){
+        $( "div" ).removeClass('inverted');
+		$( "a" ).removeClass('inverted');
+		$("body").css("background", "white");
+		$("body").css("color", "black");
+		$("h1").css("color", "black");
+		$('.invert').text('Dark Mode');
+		inverted = false;
+	  }
+	  else{
+		$( "div" ).addClass('inverted'); 
+		$( "a" ).addClass('inverted');		
+		$("body").css("background", "black");
+		$("h1").css("color", "white");
+		$('.invert').text('Light Mode');
+		inverted = true;
+	  }
     }
   
