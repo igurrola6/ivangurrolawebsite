11842e2ae21acb2c95c6f375e9e4b8f19eebeae9
igurrola6 <igurrola6@gmail.com>
2021-06-02 17:56:24 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-$(function() { // Makes sure that your function is called once all the DOM elements of the page are ready to be used.
+$(document).ready(function() { // Makes sure that your function is called once all the DOM elements of the page are ready to be used.
     $('.hide').click(clickedHide);
 	$('.show').click(clickedShow);
   })
