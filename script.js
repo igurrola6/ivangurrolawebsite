86efa6d147b3e286676023f971fff7b57c1d42a3
igurrola6 <igurrola6@gmail.com>
2021-06-05 00:54:20 -0700

    Add "Doge" section

--- script.js
+++ script.js
@@ -3,9 +3,15 @@ $(document).ready(function() { // Makes sure that your function is called once a
     $('.hide').click(clickedHide);
 	$('.show').click(clickedShow);
 	$('.invert').click(clickedInverted);
+	$('.transition').click(appear);
   })
   var inverted = true;
-  $(document).on('click', '#a', function() { alert("hello"); });
+  var on = true;
+  
+  $(document).on('click', '#a', function() { 
+	 $('#a').transition('shake');
+    
+	});
   
    function clickedHide() {
       $( "#a" ).hide();
@@ -13,6 +19,17 @@ $(document).ready(function() { // Makes sure that your function is called once a
    function clickedShow() {
       $( "#a" ).show();
     }
+   function appear(){
+	   
+	   if (on == true){
+	    $("h1").css("visibility", "collapse");
+		on = false;
+	   }
+	   else{
+		$("h1").css("visibility", "visible");   
+		on = true;
+	   }
+   }
 	
    //this is for testing, it is much better to switch css files, one for dark mode and one for light mode 
    function clickedInverted() {
